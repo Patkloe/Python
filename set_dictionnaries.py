c478409717d7289ee97c118970e9591c2dabff05
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-07-14 09:24:36 -0500

    Create set_dictionnaries.py

---
+++ set_dictionnaries.py
@@ -0,0 +1,17 @@
+thisset = {"apple", "banana", "cherry"}   # declararation set
+print(thisset)
+nbre = len(thisset)
+print(nbre)
+for i in thisset:
+ print(i)
+dict1 = {"a":"test",1:2,"c":"decouvre"} #declaration dictionnaire
+for j in dict1:
+ print(dict1[j])
+cle = dict1.keys()  #recupere cles dictionnaire
+val = dict1.values() # recupere valeurs dictionnaire
+print(cle)
+print(val)
+
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+# Refresh this page to see the change in the result.
