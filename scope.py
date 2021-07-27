f94d7a2b23577ff6faab5b7e294306bb72aa69c4
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-07-27 08:33:21 -0500

    Create scope.py

---
+++ scope.py
@@ -0,0 +1,11 @@
+def myfunc():
+  x = 300
+  y = 0
+  for i in range(5):
+    y = y + 1
+  def myinnerfunc():
+   #if(i == 5):    variable i it is not visible here
+    print(x)
+  myinnerfunc()
+
+myfunc()
