4f045cd83be2359ed2873155656b598e8dd0591e
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-07-28 12:26:20 -0500

    Update class.py

--- class.py
+++ class.py
@@ -1,1 +1,15 @@
-
+class MyClass:
+  x = 5
+  def __init__(self,name,age):
+   
+   self.name = name
+   self.age = age
+  def imprime(self):
+   print("je m'appelle " + self.name + ", j'ai " + str(self.age) + "ans")
+y = "Test"
+print(MyClass)
+print(y)
+P1 = MyClass("Patrick",37)
+print(P1.x)
+P1.imprime()
+#print(P1.y)
