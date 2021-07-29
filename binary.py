c25d4683dcc2dc36ed3f6d10229318ce1a94b500
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-07-28 22:54:31 -0500

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,7 +1,11 @@
-
+#Recursive
 def DecimalToBinary(num):
      
     if num >= 1:
         DecimalToBinary(num // 2)
     print(num % 2, end = '')
 DecimalToBinary(5)
+#in built fonction
+def decimalToBinary(n):
+    return bin(n).replace("0b", "")
+print(type(decimalToBinary(4)))
