836a424af95c7a20de16ef13917c21a001878512
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-09-02 12:50:37 -0500

    Update class.py

--- class.py
+++ class.py
@@ -13,3 +13,17 @@ P1 = MyClass("Patrick",37)
 print(P1.x)
 P1.imprime()
 #print(P1.y)
+"""
+       Another version
+       
+       class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name + "  " + " I am : " + str(self.age))
+
+p1 = Person("John", 36)
+p1.myfunc()
+"""
