346c38a29f2b3175a016d55a7cc77d16ccb1161b
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-09-03 20:07:47 -0500

    Update navigate_array_list.py

--- navigate_array_list.py
+++ navigate_array_list.py
@@ -1,1 +1,15 @@
+x = 1
+y = 2.8
+z = 1j
+s = "abc"
 
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(s))
+tab = ["cacao","cafe","coton","banane","cola","coco"]
+for i in tab :
+  print(i)
+size = len(tab)
+for j in range(size) :
+  print(tab[j])
