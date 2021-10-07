15d541f99bb01aa12bd5e3d45a60832ea1b60454
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-10-07 07:20:21 -0500

    Create list.js
    build a list with constructor list()

---
+++ list.js
@@ -0,0 +1,7 @@
+thislist = list(("apple", "banana", "cherry")) # build a list with constructor list()
+for i in thislist :   # navigate through the element of the list
+ print(i)
+nbre = len(thislist) 
+for j in range(nbre) : # navigate through the list index
+ print(thislist[j])
+ 
