0ff001f76b41c33102f55ffeff4baa4a36393695
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-10-10 09:00:03 -0500

    Create dict_keys()_values().py
    to convert to a list elements we get from dictionaries  methods  Keys()  and values() then make sure index of list are the same.   on convertit en liste / tableau les resultats des methodes keys()/values() et on s'assure que les tableaux generees auront les meme index.

---
+++ dict_keys()_values().py
@@ -0,0 +1,13 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.values()
+z = car.keys()
+print(x)
+print(z)
+#y = list(x) convertir en list
+#print(y)
+# u = list(z) convertir en list
