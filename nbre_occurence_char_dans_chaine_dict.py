8be2f49bc22a9a28905d3ed1646263b001fac595
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-10-14 20:12:26 -0500

    Update nbre_occurence_char_dans_chaine_dict.py

--- nbre_occurence_char_dans_chaine_dict.py
+++ nbre_occurence_char_dans_chaine_dict.py
@@ -6,3 +6,31 @@ nouv = {}   # on initialise un dictionnaire qui va recuperer le nombre d'occuren
 for j in test: # pour chaque charactere dans la chaine, le dictionnaire
  nouv[j] = test.count(j) # on compte le nombre d'apparitions dans la chaine on aura pour cle dans le dictionnaire le caharactere et pour valeur, le nombre d'occurence dans la chaine.
 print(nouv)
+
+# exercise
+
+given a string, we will print a string with the number of times each character appears in the given string
+
+test = "sdstdstydt  dfgdfgshg"
+def const(val): # function to build dictionary
+ dic = {}
+ for i in val:
+  dic[i] = val.count(i) # count each occurence in the string will be the value of the key in the dictionnary
+ print(dic)
+ return dic
+def ecrire(x): # function to get the dictionnary, extract keys and values
+ u = list(x.keys()) # extract keys cast the return as a list
+ v = list(x.values()) # extract values cast the return as a list
+ a = len(u)
+ b = len(v)
+ print(f"{a} keys")
+ print(f"{b} values")
+ f = ""
+ for k in range(a):
+  val = v[k]
+  while val > 0:
+   f = f + u[k]
+   val = val - 1
+ print(f) 
+  
+ecrire(const(test))
