62631e69974d68f4cbbc5e9228c55da221fa8ec9
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-11-05 10:22:24 -0500

    reverse string/list

---
+++ list_string_reverse.py
@@ -0,0 +1,14 @@
+#def retournelist(tab):
+ #return tab[::-1]
+ #return tab.reverse()
+
+essai = ["patrick","Zossie"]
+essai.reverse() # inverse list
+test = essai[::-1] # inverse list
+tst = "diffstring/list" #define the string
+rev = tst[::-1]
+#tst.reverse()  non utilisable sur string
+print(test)
+print(essai)
+print(rev)
+print(tst)  # does not work on string
