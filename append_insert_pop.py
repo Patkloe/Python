5b9ac33af428bdaafbfb7d02ac968f52b637078f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-11-08 07:07:26 -0600

    Create append_insert_pop.py
    list methods  insert(), append()  to add elements in a list.     pop() to take out an element in the list, now depending of the argument we can have a specification

---
+++ append_insert_pop.py
@@ -0,0 +1,11 @@
+fruits = ['apple', 'banana', 'cherry']
+print(fruits)
+rec = fruits.pop(0) # remove an element from the list, without argument () remove the last element of the list, with argument (a), remove the specified element
+print(fruits)
+print(rec)
+ajout = "clementine"
+fruits.append(ajout) # add an element at the end of the list
+print(fruits)
+ins = "Ginger"
+fruits.insert(0,ins) # add an element in the list at the specified location
+print(fruits)
