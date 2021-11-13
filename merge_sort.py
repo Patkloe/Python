f9e9ce5fa85e17f525b9f8a14d85f1e3259421c2
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-11-13 07:42:03 -0600

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -15,3 +15,54 @@ def trier(l):
  else:
   l1 = trier(l[0:int(n-1)/2])
   l2 = trier(l[int(n-1/2):n])
+  
+  # version 1
+  def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        mergeSort(left)
+        mergeSort(right)
+        #myList = list()
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              #myList.append(left[i])
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                #myList.append(right[j])
+                j += 1
+                # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            #myList.append(left[i])
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            #myList.append(right[j])
+            j += 1
+            k += 1
+        #mergeSort(left)
+        #mergeSort(right)
+
+myList = [54,26,93,17,77,31,44,55,20,5,4,3,2,1]
+mergeSort(myList)
+print(myList)
