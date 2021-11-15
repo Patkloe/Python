de7727b61a5ecd8db446b80b3e794d15e86ea9b6
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-11-15 08:37:09 -0600

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -46,3 +46,35 @@ def mergeSort(myList):
         return myList
 tab = [54,26,93,17,77,31,44,55,20,5,4,3,2,1]
 print(mergeSort(tab))
+'''
+def mergesort(myList):
+ if len(myList) > 1:
+  mid = len(myList)//2
+  left = myList[:mid]
+  right = myList[mid:2]
+  mergesort(left)
+  mergesort(right)
+  i = 0 # left 
+  j = 0 # right
+  k = 0 # array sorted
+  while i < len(left) and j < len(right):
+   if left[i] <= right[j]:
+    myList[k] = left[i]
+    i = i + 1
+   else:
+    myList[k] = right[j]
+    j = j + 1
+   k = k + 1
+  while i < len(left):
+   myList[k] = left[i]
+   i = i +1
+   k = k + 1
+  while j < len(right):
+   myList[k] = right[j]
+   j = j + 1
+   k = k + 1
+  return myList
+''' else:
+  return myList'''
+tab = [54,26,93,17,77,31,44,55,20,5,4,3,2,1]
+print(mergesort(tab))   '''
