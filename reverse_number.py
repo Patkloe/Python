c05046c2201cb3cb847b6c8612b89676a269f86a
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-02-06 21:00:16 -0600

    Create reverse_number.py
    python program to reverse a number

---
+++ reverse_number.py
@@ -0,0 +1,8 @@
+def reverse(n):
+ result = 0       # on initialise result a 0
+ while n != 0:     # loop sur n , tant que n different de 0
+  result *= 10      #  result est multiplie par 10
+  result += n % 10   # a result on ajoute le reste de la division de n par 10
+  n //= 10             # n est reduit a la valeur entiere de la division par 10
+ return result
+ 
