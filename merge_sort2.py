f6302f868f1455e2ffcb4fdbf6f51839b7f58d05
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-02-12 04:24:18 -0600

    Update merge_sort2.py

--- merge_sort2.py
+++ merge_sort2.py
@@ -54,4 +54,38 @@ if __name__ == '__main__':
     printList(arr)
     mergeSort(arr)
     print("Sorted array is: ", end="\n")
-    printList(arr)
+    printList(arr)           # This version is contributed by Mayank Khanna
+    
+    
+    # version 0.2   contributed by  Patrick Motsebo
+    def mergesort(myList):
+ if len(myList) > 1:
+  mid = len(myList)//2
+  left = myList[:mid]
+  right = myList[mid:]
+  mergesort(left)
+  mergesort(right)
+  i = 0 # left 
+  j = 0 # right
+  k = 0 # array sorted
+  while i < len(left) and j < len(right):
+   if left[i] <= right[j]:
+    myList[k] = left[i]
+    i = i + 1
+   else:
+    myList[k] = right[j]
+    j = j + 1
+   k = k + 1
+  while i < len(left):
+   myList[k] = left[i]
+   i = i +1
+   k = k + 1
+  while j < len(right):
+   myList[k] = right[j]
+   j = j + 1
+   k = k + 1
+  return myList
+ else:
+  return myList
+tab = [6,5,4,9,8,7,3,2,1,0]
+print(mergesort(tab))
