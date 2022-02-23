19e09c49434bec688de460f95fd896c10b236a2c
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-02-22 23:31:36 -0600

    Create list_slicing.py
    how to select a part of a list

---
+++ list_slicing.py
@@ -0,0 +1,11 @@
+tab = [1,2,3,4,5,6,7]
+tab1 = tab[:]
+tab2 = tab[::]
+tab3 = tab[:2]
+tab4 = tab[::2]
+tab5 = tab[1:6:2] # start index 1, end : index 6, jump index 2
+print(tab1)
+print(tab2)
+print(tab3)
+print(tab4)
+print(tab5)
