0755e5c31e2dc51851629f8e12868cdde3681062
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-02-24 11:33:20 -0600

    Update recupparam.py

--- recupparam.py
+++ recupparam.py
@@ -3,3 +3,12 @@ def recuppram(*args):
   print(i)
 
 recuppram("vouloir","effet","decouvrir", "tester", "victoire")
+
+# version to retrieve the number of args
+
+def recuppram(*args):
+ for i in args :
+  print(i)    
+ print(len(args)) # get the number of argument in the function
+
+recuppram("vouloir","effet","decouvrir", "tester", "victoire")
