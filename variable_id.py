01c0bceb97304a700b1a1a013dd16280a0d623be
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-09 13:57:53 -0600

    Create variable_id.py
    Just to get to id of a variable, in python a variable is to represent a memory location

---
+++ variable_id.py
@@ -0,0 +1,10 @@
+thisset = {"apple", "banana", "cherry"}
+thisset.add("orange")
+print(thisset)
+thisset.remove("orange")
+print(thisset)
+val = thisset
+print(id(val))
+print(id(thisset))
+voir = "local"
+print(id(voir))
