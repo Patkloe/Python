1d8f30f535137fb6dd412fa07d39b818ec9c52dd
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-15 14:05:00 -0500

    Update recupparam.py

--- recupparam.py
+++ recupparam.py
@@ -12,3 +12,14 @@ def recuppram(*args):
  print(len(args)) # get the number of argument in the function
 
 recuppram("vouloir","effet","decouvrir", "tester", "victoire")
+
+
+
+# other version :
+def language(**args):
+    """Demo funciton of arbitrary keyword arguments"""
+    for a in args:
+        print(a, args[a])
+        
+language(country="NL", code="+31")
+ 
