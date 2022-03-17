c8589068bfa2fffeea4e1c971ccb4dbad5cbdad5
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-17 13:42:05 -0500

    Update binary.py

--- binary.py
+++ binary.py
@@ -9,3 +9,22 @@ DecimalToBinary(5)
 def decimalToBinary(n):
     return bin(n).replace("0b", "")
 print(type(decimalToBinary(4)))
+  # other version
+  
+def binaire(n):
+ res = ""
+ while n > 0 :
+  res += str(n%2)
+  n//=2
+ return res[::-1]
+ 
+print(binaire(68))
+
+"""def decimal(n):
+ res = 0
+ for i in range[len(str(n))]:
+  res = res + n[i]*2**int(n[i])
+ return res
+ 
+print(decimal(100))"""
+  
