5c64559aad1760d20537b4d1fc9988640cc1e23e
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-19 11:57:14 -0500

    Update recupparam.py

--- recupparam.py
+++ recupparam.py
@@ -33,3 +33,18 @@ def recup(*args):
   print(x[0],x[1],sep="-")
  return res
 recup("test","essai","decouv","victoire")
+
+#sample
+def recup(*args):
+ res=[]
+ for i in range(len(args)):
+  for j in range(i+1,len(args)):
+   res.append([args[i],args[j]])
+ for x in res:
+  #print(x[0],x[1],sep="-")
+  taille1 = len(x[0])
+  taille2 = len(x[1])
+  print(x[0],x[1],sep="-")
+  print(taille1,taille2,sep="-")
+ return res
+recup("test","essai","decouv","victoire")
