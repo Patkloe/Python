51565e7c8d4946b33d2868fa4e0259c57df58cda
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-22 13:12:52 -0500

    Update operations_listes_link.py

--- operations_listes_link.py
+++ operations_listes_link.py
@@ -27,3 +27,14 @@ print(tab)
 del tab[:]
 print(tab)
 '''
+# delete white space
+sentence = ' hello  apple  '
+sentence.strip()
+print(sentence)
+dec = sentence.split(" ")
+#sentence.remove(' ')
+print(dec)
+del dec[2]
+print(dec)
+nouv ="".join(dec)
+print(nouv)
