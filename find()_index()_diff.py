6c7bf0156678876b62e78c4e3b366e440fcac20f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-23 00:42:15 -0500

    Create find()_index()_diff.py

---
+++ find()_index()_diff.py
@@ -0,0 +1,7 @@
+s1 = "just learn string functions"
+s2 = "just"
+s1.find(s2)      #output 0
+s3 = "decouvre"
+s1.find(s3)      #output   -1
+s.index(s3)      # will raise a python error
+# s1.find("texte recherche",deb,fin)   as s1.index()
