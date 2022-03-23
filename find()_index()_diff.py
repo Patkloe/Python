4dbaa9ec72d0866b1caf544705dfbb386e7a53f9
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-23 08:53:45 -0500

    Update find()_index()_diff.py

--- find()_index()_diff.py
+++ find()_index()_diff.py
@@ -7,3 +7,16 @@ s.index(s3)      # will raise a python error
 # s1.find("texte recherche",deb,fin)   as s1.index()
 # index() return the index where the substring starts in the string
 #rindex() return the end of index where we have the substring
+
+
+# sample
+
+tab = "veux decouvrir Python Python"
+print(tab.rindex("Python"))
+test =tab[4:]
+suit =tab[:4]
+form1 = test + suit
+form2 = suit + test
+print(form1)
+print(form2)
+print(tab.find("veux"))
