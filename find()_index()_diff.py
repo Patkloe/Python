71ed46580f3e560546e4d44cfb4758172f3128b8
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-23 00:47:26 -0500

    Update find()_index()_diff.py

--- find()_index()_diff.py
+++ find()_index()_diff.py
@@ -5,3 +5,5 @@ s3 = "decouvre"
 s1.find(s3)      #output   -1
 s.index(s3)      # will raise a python error
 # s1.find("texte recherche",deb,fin)   as s1.index()
+# index() return the index where the substring starts in the string
+#rindex() return the end of index where we have the substring
