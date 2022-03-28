8d2593701f620ff3dfc91ba0ed2214453d57271c
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-03-28 14:04:37 -0500

    Create majority.py
    given an array with a multitude of same element, write a function to return the number of alement that appears the most.

---
+++ majority.py
@@ -0,0 +1,13 @@
+def majority():
+ tab=[1,1,1,2,3,4,5,2,2,2,3,5,2,1,1,1]
+ dic={}
+ maj=0
+ for i in range(len(tab)):
+  dic[tab[i]]=tab.count(tab[i])
+  if dic[tab[i]]>maj:
+   maj=dic[tab[i]]
+   ind=i
+ print(dic)
+ return maj,i
+ 
+print(majority())
