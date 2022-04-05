105b631824779916842785a40fdeebc7e937f3d0
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-04-05 07:15:53 -0500

    Create slicing.py
    slicing array

---
+++ slicing.py
@@ -0,0 +1,21 @@
+#rotate array by slicing
+li=[1,2,3,4,5]
+l1=li[2:]
+l2=li[:2]
+print(l1)
+print(l2)
+rot=l1+l2
+print(rot)
+#inverse
+inv1=li[-2:]
+inv2=li[:-2]
+print(inv1)
+print(inv2)
+invrot=inv1+inv2
+print(invrot)
+tab=[-1,2,0,5,3,4]
+print(min(tab))
+alph=["a","v","l","o"]
+print(min(alph))
+test=li[:-1:]
+print(test)
