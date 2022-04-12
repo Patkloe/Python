1d304ceb9b55eef34978b68a60bb7b9a569817a9
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-04-12 07:47:04 -0500

    Update copy_fobj.py

--- copy_fobj.py
+++ copy_fobj.py
@@ -6,3 +6,21 @@ print(cop)
 ex[0] = "W"   # we modify the source
 print(ex)     # source changes
 print(cop)    # the copy does not change.
+# compare list between list, set, and dictionnaries
+test = "exemple, voir"
+ex = list(test)
+print(ex)
+cop = ex[:]   # we copy the object
+print(cop)
+ex[0] = "W"   # we modify the source
+print(ex)     # source changes
+print(cop)    # the copy does not change.
+print("tableau")
+print(cop==ex)
+set1={1,2,3}
+set2={1,2,3}
+print("set")
+print(set1==set2)
+dict1={1:"a",2:"b",3:"c"}
+dict2={1:"a",2:"b",3:"c"}
+print(dict1==dict2)
