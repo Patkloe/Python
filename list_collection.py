867ccf02a8e1cd11b1fba7cd861594e720d1e24b
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-04-12 06:52:29 -0500

    Create list_collection.py

---
+++ list_collection.py
@@ -0,0 +1,8 @@
+val={1,2,3,4,5,6,7,8,9,10,11,12}   # define a set
+lst=[x for x in val if x%2==0]  #define a list with values from val with a speciality
+print(lst)   #print the list
+dec=[x for x in range(12)] # another list collection depending of specific values
+print(dec)
+print(len(val))
+#nouv=[x for x in val.values()]
+#print(nouv)
