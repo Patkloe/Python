34de41379b1668fb4e126f8c1b0cd8b5dc4b224a
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-05-23 12:29:55 -0500

    Update try_except_conv.py
    how to use (try/except) to handle errors generated by Python

--- try_except_conv.py
+++ try_except_conv.py
@@ -4,3 +4,12 @@ try:
  print("conversion success")
 except:
  print("impossible")
+
+
+tab=[1,3,4,5,6,7,8,9]
+try:
+ tab.index(10)
+ print("this value is on the dictionary")
+except:
+ print("value not in the dictionnary")
+ 
