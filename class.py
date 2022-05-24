b2fa584787ed960ba52890878f6f3a059ebea30e
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-05-24 07:30:41 -0500

    Update class.py

--- class.py
+++ class.py
@@ -27,3 +27,30 @@ P1.imprime()
 p1 = Person("John", 36)
 p1.myfunc()
 """
+# another version
+
+class Node:
+   def __init__(self,data):
+    self.right = None
+    self.left = None
+    self.data = data
+   def insert(self,data):
+     if self.data:
+        if self.data > data:
+           if self.left is None:
+             self.left = Node(data)
+           else:
+             self.left.insert(data)
+        else:
+          if self.right is None:
+             self.right = Node(data)
+          else:
+             self.right.insert(data)
+     else:
+          self.dat = data
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+print("Success recording")
+#print(Node(6).right)
