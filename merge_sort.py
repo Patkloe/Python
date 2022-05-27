94072b557821002b04728d80dfd51310a45b9ce8
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-05-27 07:37:58 -0500

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -81,4 +81,40 @@ print(mergeSort(tab))
 tab = [6,5,4,9,8,7,3,2,1,0]
 print(mergesort(tab))
 3 test a revoir
+# merge sort, good version.  variable i,j,k initiation apres l'appel recursif de mergesort()
+def mergesort(tab):
+ if len(tab)>1:
+  mid=len(tab)//2
+  left=tab[:mid]
+  right=tab[mid:]
+  mergesort(left)
+  mergesort(right)
+  i=0
+  j=0
+  k=0
+  while i<len(left) and j<len(right):
+   if left[i]<right[j]:
+    tab[k]=left[i]
+    i+=1
+   else:
+    tab[k]=right[j]
+    j+=1
+   k+=1
+  while j<len(right):
+   tab[k]=right[j]
+   j+=1
+   k+=1
+  while i<len(left):
+   tab[k]=left[i]
+   i+=1
+   k+=1
+  return tab
+ else:
+  return tab
+
+mat=[10,2,3,1,5,7,6,4,9,8,15,12,35,32,30]
+print(mergesort(mat))
+
+
+
 
