73cf7236494959301810d37ccdcbc900589c5b10
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-03 07:05:09 -0500

    Update bestsqrt.py

--- bestsqrt.py
+++ bestsqrt.py
@@ -11,3 +11,16 @@ def bestsqrt(x):
   
 print(bestsqrt(16))
    
+# version 000000.1
+def bstsqrt(x):
+ import math
+ sol=[]
+ if x>1:
+  for i in range(1,x):
+   if math.floor(i*i)<=x:
+    sol.append(i)
+  return max(sol)
+ else:
+  return 0
+
+print(bstsqrt(2))
