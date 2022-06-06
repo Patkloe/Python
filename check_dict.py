1f0ec8197b72b9ff4fb227f66cc91ecea6757039
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-06 07:16:36 -0500

    Update check_dict.py

--- check_dict.py
+++ check_dict.py
@@ -24,3 +24,15 @@ def verif_dict(tab):
  return sol
 tab=[1,1,1,3,4,5,5,8,8,7,7]
 print(verif_dict(tab))
+#to check tonight
+def verif_dict(tab):
+ sol={}
+ for i in tab:
+  if i in sol:
+   sol[i]+=1
+  else:
+   sol[i]=1
+ # here condition to display the result
+ return sol
+tab=[1,1,1,3,4,5,5,8,8,7,7]
+print(verif_dict(tab))
