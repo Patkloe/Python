0ee3982a49db5064c93ced3b61a1356f1093326f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-09 08:56:24 -0500

    Create list_tuple_set.py

---
+++ list_tuple_set.py
@@ -0,0 +1,20 @@
+def decouv():
+ var="venez decouvrir Python"
+ aff=list(var)
+ print("This is the String")
+ print(str(aff))
+ print("This is the set")
+ print(set(var))
+ #print(dict(var))
+ print("This is the Tuple")
+ print(tuple(var))
+ return ""
+print(decouv())
+
+
+
+val=(1,2,3,1,4)
+print("This is the tuple")
+print(val)
+#rev=val.add(5)
+#print(rev)
