4728a34e375023db3cb0f573d7f67a5f74740090
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-15 07:05:35 -0500

    Update set_list.py
    list and set constructor, then how to access set elements

--- set_list.py
+++ set_list.py
@@ -6,3 +6,21 @@ print(test)
 print(test)
 testl=list(ex1)
 print(testl)
+# Python program to demonstrate
+# Accessing of elements in a set
+  
+# Creating a set
+set1 = set(["Geeks", "For", "Geeks"])
+print("\nInitial set")
+print(set1)
+  
+# Accessing element using
+# for loop
+print("\nElements of set: ")
+for i in set1:
+  if i=="Geeks":
+    print(i+" "+"bingo", end=" ")
+  
+# Checking the element
+# using in keyword
+print("Geeks1" in set1)
