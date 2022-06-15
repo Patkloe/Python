7008445ed3c4aa37a0de6b023dc1e37459609a17
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-15 07:51:20 -0500

    Update list_max_element_set.py

--- list_max_element_set.py
+++ list_max_element_set.py
@@ -30,3 +30,19 @@ for i in tab:
  
  
 print(i)
+#  another test
+tab=[1,1,2,2,1,2,2,2,2,3,3,4,2,2,2]
+max=len(tab)/2
+nbre=0
+const=set()
+for i in tab:
+ if i in const:
+  nbre+=1
+  if nbre>max:
+   max=nbre
+   index=i
+ else:
+  const.add(i)
+ 
+ 
+print(i)
