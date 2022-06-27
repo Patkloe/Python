cc00130821799075dd83cc0e964891ac50841170
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-27 06:56:17 -0500

    Update merge.py
    different version of Merge sort algorithm, complexity "nlogn"

--- merge.py
+++ merge.py
@@ -23,5 +23,36 @@ tab1 = [2,3,5,6,7]
 tab2 = [1,9,6,8,4,0]
 print(merge(tab1,tab2))
 # a bosser
+#### Other version ######
+def mergesort(tab):
+ if len(tab)>1:
+  mid=(len(tab))//2
+  left=tab[:mid]
+  right=tab[mid:]
+  mergesort(left)
+  mergesort(right)
+  i=j=k=0 #left
+  while i<len(left) and j<len(right):
+   if left[i]<right[j]:
+    tab[k]=left[i]
+    i+=1
+   else:
+    tab[k]=right[j]
+    j+=1
+   k+=1
+  while i<len(left):
+   tab[k]=left[i]
+   i+=1
+   k+=1
+  while j<len(right):
+   tab[k]=right[j]
+   j+=1
+   k+=1
+  return tab
+ else:
+  return tab
+
+tab=[13,14,15,9,7,5,3,6,7,4,2,1,0]
+print(mergesort(tab))
 
 
