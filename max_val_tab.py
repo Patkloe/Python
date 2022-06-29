291213cb8aac3242fc66fec9e6f17f8932a0c2e3
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-06-29 07:25:54 -0500

    Update max_val_tab.py

--- max_val_tab.py
+++ max_val_tab.py
@@ -13,3 +13,28 @@ def Maxtab(tab):
  print(reserv)
  return i,maxv
 print(Maxtab("voici un exemple"))
+
+# function to get the max value in a string or a list,  then another function to retrieve number of argument for a function
+
+def Maxtab(tab):
+ reserv={}
+ maxv=0
+ ind=""
+ for i in tab:
+   if i in reserv:
+     reserv[i]+=1
+     if reserv[i]>maxv:
+      maxv=reserv[i]
+      ind=i
+   else:
+    reserv[i]=1
+ print(reserv)
+ return i,maxv
+print(Maxtab("voici un exemple ici ce en ligne"))
+
+# There is the function to get amount of arguments for a function
+
+def sizearg(*args):
+ recup=len(args)
+ return recup
+print(sizearg("test","developper","bosser","facilite","twitter","observe"))
