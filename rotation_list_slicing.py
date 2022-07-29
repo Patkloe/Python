20cf499a6439cf107dac3617a0c851f501bb382c
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-07-29 09:37:33 -0500

    Update rotation_list_slicing.py
    dynamic variable name

--- rotation_list_slicing.py
+++ rotation_list_slicing.py
@@ -6,3 +6,16 @@ print(tab)
 print(tab1)
 print(tab2)
 print(tab3)
+
+#dynamic variable name
+
+for n in range(0, 7):
+    globals()['strg%s' % n] = 'Hello'
+# strg0 = 'Hello', strg1 = 'Hello' ... strg6 = 'Hello'
+
+for x in range(0, 7):
+    globals()[f"variable1{x}"] = f"Hello the variable number {x}!"
+
+
+print(variable16)
+print(strg0)
