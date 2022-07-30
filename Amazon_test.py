79f6ba60b4bc6dd4c340c81864d74ccdbbcf021d
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-07-30 11:20:55 -0500

    Create Amazon_test.py
    Given an array(0-based indexing), you have to find the max sum of i*A[i] where A[i] is the element at index i in the array. The only operation allowed is to rotate(clock-wise or counter clock-wise) the array any number of time

---
+++ Amazon_test.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+def compte(tab):
+ sum1=0
+ for i in range(len(tab)):
+  sum1=sum1+(tab[i]*i)
+ #print(sum1)
+ return sum1
+#tab=[8,3,1,2]
+def max_sum(a,n):
+ sum=0
+ cal=0
+ for i in range(n):
+  locals()['a%s'%i]=a[i:]+a[:i]
+ '''for i in range(n):
+  print(locals()['a%s'%i])'''
+ for i in range(n):
+  #cal=cal+(locals()['a%s'%i][i]*i)
+  cal=compte(locals()['a%s'%i])
+  if cal>sum:
+   sum=cal
+ return sum
+ #print(sum)
+ 
