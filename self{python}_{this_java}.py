3756100ec2de2a6ac87399360a9f8cab138ca9d4
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-08-17 07:23:12 -0500

    Update self{python}_{this_java}.py

--- self{python}_{this_java}.py
+++ self{python}_{this_java}.py
@@ -30,3 +30,41 @@ obj=test()
 obj.test_func()
 
 '''
+# another sample
+
+class test:
+ def __init__(self):
+  print("this is how you start")
+ def test_func(self,x,y):
+  print("This is for the best")
+  self.x=x
+  self.y=y
+  print(self.x)
+  print(self.y)
+obj=test()
+obj.test_func("Patrick","Motsebo")
+
+''' how we use "self" in python, like "this" in Java
+class test:
+ def __init__():
+  print("this is how you start")
+ def test_func(self):
+  print("This is for the best")
+obj=test()
+obj.test_func()
+
+error generated during running time 
+Traceback (most recent call last):
+  File "./prog.py", line 7, in <module>
+TypeError: test_func() takes 0 positional arguments but 1 was given
+waiting for an argument when it is defined, that's why we need to use
+"self" as the default argument
+class test:
+ def __init__(self):
+  print("this is how you start")
+ def test_func(self):
+  print("This is for the best")
+obj=test()
+obj.test_func()
+
+'''
