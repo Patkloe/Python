78b38345fa1adb0d3de6e1869e897ed8785924ff
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-09-13 00:22:37 -0500

    Update mergeweight.py

--- mergeweight.py
+++ mergeweight.py
@@ -60,7 +60,7 @@ def Evalue(tab):
 tab=[4,2,9,10,3,7]
 print(Evalue(tab))
 
-#good version
+#good version  for loop
 def compare(x,y):
  if x<y:
   return True
@@ -93,4 +93,39 @@ def mergeweight(tab):
  return tab
 tab=[8,9,1,4,3,5,7,8,2,3,4]
 print(mergeweight(tab))
+# good version while loop
+def compare(x,y):
+ if x<y:
+  return True
+ else:
+  return False
+  
+def mergeweight(tab):
+ val=0
+ i=0
+ while i<len(tab):
+  #j=i+1
+  if i+1<len(tab) and compare(tab[i],tab[i+1]):
+   val=tab[i]+tab[i+1]
+   tab.pop(i+1)
+   tab[i]=val
+   print(tab)
+   mergeweight(tab)
+  #elif j<len(tab) and not compare(tab[i],tab[j]):
+  # i+=1
+  i+=1
+ ''' for i in range(len(tab)):
+   #j=i+1
+   if i+1<len(tab) and compare(tab[i],tab[i+1]):
+    val=tab[i]+tab[i+1]
+    tab.pop(i+1)
+    tab[i]=val
+    print(len(tab))
+    print(tab)
+    mergeweight(tab)'''
+ return tab
+tab=[8,9,1,4,3,5,7,8,2,3,4]
+print(mergeweight(tab))
+  
+
   
