94f4e897cea4e8b2cc261aff6d45ed976aca77be
Patkloe <52906039+Patkloe@users.noreply.github.com>
2022-09-15 06:42:15 -0500

    Create permut_list.py
    to permut element of a list

---
+++ permut_list.py
@@ -0,0 +1,16 @@
+test={'a':'a','b':'b','c':'c'}
+test1={'b':'b','a':'a','c':'c'}
+print(test==test1)
+
+tab=[1,5,8,9,4,3,6,2]
+tabun=tab[1:]+tab[:1]
+tabdeux=tab[2:]+tab[:2]
+tabtrois=tab[3:]+tab[:3]
+tabquatre=tab[4:]+tab[:4]
+tabcinq=tab[5:]+tab[:5]
+print(tab)
+print(tabun)
+print(tabdeux)
+print(tabtrois)
+print(tabquatre)
+print(tabcinq)
