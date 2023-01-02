f5147d0d7d1602a2560698d88f9ed97627b66c0a
Patkloe <52906039+Patkloe@users.noreply.github.com>
2023-01-02 01:03:41 -0600

    Update dict_keys()_values().py

--- dict_keys()_values().py
+++ dict_keys()_values().py
@@ -17,3 +17,4 @@ tr={1:2,4:5,3:2}
 print(min(sorted(tr.items()),key=lambda a:a[1]))  # we use lambda to tell the function how to return those values, items() we get two values
 print(min(sorted(tr.keys())))  # here we get just a value   for each item in the dictionary
 print(min(sorted(tr.values()))) # here we get just a value  for each item in the dictionary
+print(sorted(tr.items(),reverse=True)) #sorted reverse
