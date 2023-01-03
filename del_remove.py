fe669a85b05e32d09b6fd5ca9867af9ba8f85515
Patkloe <52906039+Patkloe@users.noreply.github.com>
2023-01-03 12:14:10 -0600

    Create del_remove.py
    to use function "remove", and "del" in Python

---
+++ del_remove.py
@@ -0,0 +1,7 @@
+test={1:"test",2:"view",3:"essai",4:"decouvre"}
+tab=[1,2,3,4,5,6,7]
+tab.remove(tab[0])
+affich=test.items()
+del test[1] # 'del', use for object, 'remove' used for list
+print(tab)  #list
+print(affich) #dictionary
