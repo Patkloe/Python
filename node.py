7e07f896bee033c258e2187e70d6e6564df0e30c
Patkloe <52906039+Patkloe@users.noreply.github.com>
2023-06-27 08:05:53 -0500

    Create node.py
    this is just an sample to create and represented a Node in Python

---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+ def __init__(self,data):
+  self.data=data
+ def __repr__(self):
+  return self.data
+  
+a=Node("A")
+print(a)
+
+# this is just an sample to create and represented a Node in Python
