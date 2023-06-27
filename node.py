3131f0071ef85f42181230644adc12de1b04edfa
Patkloe <52906039+Patkloe@users.noreply.github.com>
2023-06-27 16:49:34 -0500

    Update node.py

--- node.py
+++ node.py
@@ -9,3 +9,18 @@ a=Node("A")
 print(a)
 
 # this is just an sample to create and represented a Node in Python
+
+#sample 2
+class Node:
+ def __init__(self,data):
+  self.data=data
+  self.next=None
+ def __repr__(self):
+  return self.data
+
+a=Node("1")
+b=Node("2")
+a.next=b
+print(a)
+print(a.next.data)
+
