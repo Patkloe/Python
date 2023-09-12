9a78e79f05e9d8d9a8974e35aa2ac1aadd5c068f
Patkloe <52906039+Patkloe@users.noreply.github.com>
2023-09-11 21:17:54 -0500

    Create add_to_array_form_integer.py
    The array-form of an integer num is an array representing its digits in left to right order.
    
    For example, for num = 1321, the array form is [1,3,2,1].
    Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
    
     
    
    Example 1:
    
    Input: num = [1,2,0,0], k = 34
    Output: [1,2,3,4]
    Explanation: 1200 + 34 = 1234
    Example 2:
    
    Input: num = [2,7,4], k = 181
    Output: [4,5,5]
    Explanation: 274 + 181 = 455
    Example 3:
    
    Input: num = [2,1,5], k = 806
    Output: [1,0,2,1]
    Explanation: 215 + 806 = 1021

---
+++ add_to_array_form_integer.py
@@ -0,0 +1,16 @@
+def addToArrayForm(num, k):
+ temp="" 
+ mat=[]  
+ for i in num:
+  temp=temp+str(i)
+ print(temp)
+ ret=int(temp)+k
+ val=str(ret)
+ for j in val:
+  mat.append(j)
+ return mat
+ 
+ 
+num = [2,1,5]
+k = 806
+print(addToArrayForm(num, k))
