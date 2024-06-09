c8e79a64f5fd8e906e289637577b4b5684bdaff2
Patkloe <52906039+Patkloe@users.noreply.github.com>
2024-06-09 15:07:29 -0500

    Update matrice_carree_list_of_list.py
    how to access a list of list

--- matrice_carree_list_of_list.py
+++ matrice_carree_list_of_list.py
@@ -5,3 +5,22 @@ tab=[[1, 2, 3], [4, 5, 6]]
 for i in range(len(tab)):
  for j in range(len(tab[i])):
   print(tab[i][j])
+  # another sample
+tab=list(list())
+tab=[[1, 2, 3], [4, 5, 6]]
+#print(tab[1][1])
+#print(len(tab[0]))
+for i in range(len(tab)):
+ for j in range(len(tab[i])):
+  print(tab[i][j])
+trois=[[1,2,3,4],[2,3,1,1],[4,2,3,1],[1,5,7,8]]
+print(len(trois))
+games=[[1,1],[1,1]]
+print(games[0][0])
+print(games[0][1])
+print(games[1][0])
+print(games[1][1])
+for j in range(len(trois)):
+ for i in range(len(trois[j])):
+  print(trois[j][i])
+
