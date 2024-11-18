b33f71db484b887ae5e639fbc4f9159bddf5a862
Patkloe <52906039+Patkloe@users.noreply.github.com>
2024-11-18 13:22:44 -0600

    Update tuple_built.py

--- tuple_built.py
+++ tuple_built.py
@@ -21,4 +21,26 @@ for i in range(len(tab)):
    res.append(nouv)
    temp=[]
 print(res)
+tab=["tre","yui","opi","cdf","rty","mkl","klo"]
+res=[]
+temp=[]
+for i in range(len(tab)):
+ for j in range(i+1,len(tab)):
+  temp.append(tab[i])
+  temp.append(tab[j])
+  if len(temp)==2:
+   nouv=tuple(temp)
+   res.append(nouv)
+   temp=[]
+print(res)
+tup=()
+test=[]
+for i in range(len(tab)):
+  tup+=(tab[i],)
+  print(tup)
+  if len(tup)==2:
+   test.append(tup)
+   tup=()
+print(test) 
+
 
